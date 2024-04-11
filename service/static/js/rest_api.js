fba47d32e74838a85028b1f2406d4b3e00f57f36
Wenlong Dong <wd2278@nyu.edu>
2024-04-11 17:15:46 +0000

    Added static files and finish create product

---
+++ service/static/js/rest_api.js
@@ -0,0 +1,273 @@
+$(function () {
+
+    // ****************************************
+    //  U T I L I T Y   F U N C T I O N S
+    // ****************************************
+
+    // Updates the form with data from the response
+    function update_form_data(res) {
+        $("#product_id").val(res.id);
+        $("#product_name").val(res.name);
+        $("#product_img_url").val(res.img_url);
+        $("#product_description").val(res.description);
+        $("#product_price").val(res.price);
+        $("#product_rating").val(res.rating);
+        $("#product_category").val(res.category);
+        if (res.status == true) {
+            $("#product_status").val("true");
+        } else {
+            $("#product_status").val("false");
+        }
+        $("#product_likes").val(res.like);
+    }
+
+    /// Clears all form fields
+    function clear_form_data() {
+        $("#product_id").val("");
+        $("#product_name").val("");
+        $("#product_img_url").val("");
+        $("#product_description").val("");
+        $("#product_price").val("");
+        $("#product_rating").val("");
+        $("#product_category").val("");
+        $("#product_status").val("");
+        $("#product_likes").val("");
+    }
+
+    // Updates the flash message area
+    function flash_message(message) {
+        $("#flash_message").empty();
+        $("#flash_message").append(message);
+    }
+
+    // ****************************************
+    // Create a Product
+    // ****************************************
+
+    $("#create-btn").click(function () {
+
+        let name = $("#product_name").val();
+        let img_url = $("#product_img_url").val();
+        let description = $("#product_description").val();
+        let price = $("product_price").val();
+        let rating = $("#product_rating").val();
+        let category = $("#product_category").val();
+        let status = $("#product_status").val() == "true";
+        let likes = $("#product_likes").val();
+
+        let data = {
+            "name": name,
+            "img_url": img_url,
+            "description": description,
+            "price": price,
+            "rating": rating,
+            "category": category,
+            "status": status,
+            "likes": likes
+        };
+
+        $("#flash_message").empty();
+        
+        let ajax = $.ajax({
+            type: "POST",
+            url: "/products",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+        });
+
+        ajax.done(function(res){
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+    });
+
+
+    // ****************************************
+    // Update a Product
+    // ****************************************
+
+    $("#update-btn").click(function () {
+
+        let pet_id = $("#pet_id").val();
+        let name = $("#pet_name").val();
+        let category = $("#pet_category").val();
+        let available = $("#pet_available").val() == "true";
+        let gender = $("#pet_gender").val();
+        let birthday = $("#pet_birthday").val();
+
+        let data = {
+            "name": name,
+            "category": category,
+            "available": available,
+            "gender": gender,
+            "birthday": birthday
+        };
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+                type: "PUT",
+                url: `/pets/${pet_id}`,
+                contentType: "application/json",
+                data: JSON.stringify(data)
+            })
+
+        ajax.done(function(res){
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Retrieve a Product
+    // ****************************************
+
+    $("#retrieve-btn").click(function () {
+
+        let pet_id = $("#pet_id").val();
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/pets/${pet_id}`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            clear_form_data()
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Delete a Product
+    // ****************************************
+
+    $("#delete-btn").click(function () {
+
+        let pet_id = $("#pet_id").val();
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "DELETE",
+            url: `/pets/${pet_id}`,
+            contentType: "application/json",
+            data: '',
+        })
+
+        ajax.done(function(res){
+            clear_form_data()
+            flash_message("Pet has been Deleted!")
+        });
+
+        ajax.fail(function(res){
+            flash_message("Server error!")
+        });
+    });
+
+    // ****************************************
+    // Clear the form
+    // ****************************************
+
+    $("#clear-btn").click(function () {
+        $("#pet_id").val("");
+        $("#flash_message").empty();
+        clear_form_data()
+    });
+
+    // ****************************************
+    // Search for a Product
+    // ****************************************
+
+    $("#search-btn").click(function () {
+
+        let name = $("#pet_name").val();
+        let category = $("#pet_category").val();
+        let available = $("#pet_available").val() == "true";
+
+        let queryString = ""
+
+        if (name) {
+            queryString += 'name=' + name
+        }
+        if (category) {
+            if (queryString.length > 0) {
+                queryString += '&category=' + category
+            } else {
+                queryString += 'category=' + category
+            }
+        }
+        if (available) {
+            if (queryString.length > 0) {
+                queryString += '&available=' + available
+            } else {
+                queryString += 'available=' + available
+            }
+        }
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/pets?${queryString}`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            $("#search_results").empty();
+            let table = '<table class="table table-striped" cellpadding="10">'
+            table += '<thead><tr>'
+            table += '<th class="col-md-2">ID</th>'
+            table += '<th class="col-md-2">Name</th>'
+            table += '<th class="col-md-2">Category</th>'
+            table += '<th class="col-md-2">Available</th>'
+            table += '<th class="col-md-2">Gender</th>'
+            table += '<th class="col-md-2">Birthday</th>'
+            table += '</tr></thead><tbody>'
+            let firstPet = "";
+            for(let i = 0; i < res.length; i++) {
+                let pet = res[i];
+                table +=  `<tr id="row_${i}"><td>${pet.id}</td><td>${pet.name}</td><td>${pet.category}</td><td>${pet.available}</td><td>${pet.gender}</td><td>${pet.birthday}</td></tr>`;
+                if (i == 0) {
+                    firstPet = pet;
+                }
+            }
+            table += '</tbody></table>';
+            $("#search_results").append(table);
+
+            // copy the first result to the form
+            if (firstPet != "") {
+                update_form_data(firstPet)
+            }
+
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+})
